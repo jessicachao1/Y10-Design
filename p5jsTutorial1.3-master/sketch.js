b9a5b25fc5889bd7ece8b01b0006afa0cfa84443
jessicachao1 <46880406+jessicachao1@users.noreply.github.com>
2019-02-25 11:57:32 +0800

    Add files via upload

---
+++ p5jsTutorial1.3-master/sketch.js
@@ -0,0 +1,28 @@
+function setup()
+{
+  createCanvas(700,500);
+
+}
+
+function draw()
+{
+	background(255,0,0)
+
+	fill(25,33,25);
+	stroke(100,100,200);
+	strokeWeight(30);
+
+	text("Hello World",80,30);
+
+	rect(10,10,100,50);
+
+  fill(255,69,25);
+  text("Jessica Chao",10,50);
+  fill(255,33,25);
+
+	rect(10,100,100,50);
+
+	fill(255,255,255);
+
+
+}
